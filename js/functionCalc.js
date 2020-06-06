199cf6b75081b1e96c5cb6280292d27bcc4ad8ec
Eduardo Andrade <tvduardo@gmail.com>
2020-06-06 01:46:02 -0300

    i create function to my buttons

---
+++ js/functionCalc.js
@@ -0,0 +1,49 @@
+var inputElement = document.querySelector('.inputElement')
+function One(){
+    inputElement.value += 1;
+}
+function two(){
+    inputElement.value += 2;
+}
+function tree(){
+    inputElement.value += 3;
+}
+function four(){
+    inputElement.value += 4;
+}
+function five(){
+    inputElement.value += 5;
+}
+function six(){
+    inputElement.value += 6;
+}
+function seven(){
+    inputElement.value += 7;
+}
+function eight(){
+    inputElement.value += 8;
+}
+function none(){
+    inputElement.value += 9;
+}
+function zero(){
+    inputElement.value += 0;
+}
+function vezes(){
+    
+}
+function dividir(){
+    
+}
+function soma(){
+    
+}
+function subtration(){
+    
+}
+function deleted(){
+
+}
+function igual(){
+    
+}
